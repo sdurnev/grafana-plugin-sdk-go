de426b0c2797e30c5ef18b7431920748358403b7
Ryan McKinley <ryantxu@gmail.com>
2020-12-15 22:06:00 -0800

    merge master

--- measurement/action.go
+++ measurement/action.go
@@ -1,11 +1,13 @@
 package measurement
 
+// PathValue will set a single value
 type PathValue struct {
 	Path    string            `json:"path,omitempty"`    // location to write data
 	Value   interface{}       `json:"value,omitempty"`   // value to write
 	Headers map[string]string `json:"headers,omitempty"` // optional header values to write
 }
 
+// ActionCommand write the values
 type ActionCommand struct {
 	ID      string      `json:"id,omitempty"`      // Identify the command (optional)
 	Write   []PathValue `json:"write,omitempty"`   // Write values
